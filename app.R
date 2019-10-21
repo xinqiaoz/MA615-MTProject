2b835d62eab7df0c9f1ef08aeb759d804e3b598b
xinqiaoz <54417053+xinqiaoz@users.noreply.github.com>
2019-10-21 13:00:56 -0400

    Add files via upload

---
+++ app.R
@@ -0,0 +1,71 @@
+library(shiny)
+library(tidyverse)
+library(ggplot2)
+
+## Data exacting
+data<- read.csv("~/Desktop/BU/MA615/MidTermProj/Reef_Life_Survey_(RLS)#_Cryptic_Fish-cryptic_fish_surveys.csv")
+data<- data[(colSums(is.na(data)) != nrow(data))]
+
+## Data cleaning and Selection
+data<- data %>% filter(Country=="Australia")
+data<- data%>% filter(Class=="Actinopterygii")
+
+## Separate date by Year, Month, Day and Time
+data1<- data %>% separate(SurveyDate, c("Year","Month","Day"))
+data1<- data1 %>% separate(Day, c("Day","Time"), sep="T")
+
+## Factor the data
+data1$Year<- as.factor(data1$Year)
+data1$Month<- as.factor(data1$Month)
+data1$Day<- as.factor(data1$Day)
+
+## Making subsets for shiny
+data_Year<- data1 %>% group_by(Year) %>% summarize(NewDepth=mean(Depth), NewBlock=sum(Block), NewTotal=sum(Total))
+data_Family<- data1 %>% group_by(Family) %>% summarize(NewDepth=mean(Depth), NewBlock=sum(Block), NewTotal=sum(Total))
+data_Taxon<- data1 %>% group_by(Taxon) %>% summarize(NewDepth=mean(Depth), NewBlock=sum(Block), NewTotal=sum(Total))
+data_Depth<- data1 %>% group_by(Depth) %>% summarize(NewBlock=mean(Block), NewTotal=mean(Total))
+
+#######################################
+
+#### First Shiny App
+
+ui1<- fluidPage(
+    titlePanel("Reef Life data in Australia by Family"),
+    sidebarLayout(
+        sidebarPanel(
+            selectInput(inputId = "family", label = "Family", data_Family$Family)
+        ),
+        mainPanel(tableOutput("fa"))
+    )
+)
+
+
+server1<- function(input, output, session){
+    output$fa= renderTable({
+        print(filter(data_Family, Family==input$family))
+    })
+}
+
+shinyApp(ui1, server1)
+
+#### Second Shiny App
+
+ui2<- fluidPage(
+    titlePanel("Reef Life data in Australia by Year"),
+    sidebarLayout(
+        sidebarPanel(
+            selectInput(inputId = "year", label = "Year", data_Year$Year)
+        ), 
+        mainPanel(tableOutput("ye"))
+    )
+)
+
+
+server2<- function(input, output, session){
+    output$ye= renderTable({
+        print(filter(data_Year, Year==input$year))
+    })
+}
+
+shinyApp(ui2, server2)
+
